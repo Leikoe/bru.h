376079597524b7867a959a310a394ef6408b30c2
Leiko Lei <chachafrdark@gmail.com>
2022-10-18 15:56:25 +0200

    format changes

--- src/string_vector.c
+++ src/string_vector.c
@@ -4,26 +4,22 @@
 
 #include "string_vector.h"
 #include <stdlib.h>
+#include <string.h>
 
-void
-string_vector_init( struct StringVector *this, size_t capacity){
+void string_vector_init( struct StringVector *this, size_t capacity) {
     this->capacity = capacity;
     this->size     = 0;
     this->strings  = malloc( capacity * sizeof( char * ) );
 }
 
-void
-string_vector_free( struct StringVector *this )
-{
+void string_vector_free( struct StringVector *this ) {
     for ( int i = 0; i < (int)( this->size ); i++ ) {
         free( this->strings[i] );
     }
     free( this->strings );
 }
 
-void
-string_vector_add( struct StringVector *this, const char *begin, const char *end )
-{
+void string_vector_add( struct StringVector *this, const char *begin, const char *end ) {
     if ( this->capacity == this->size ) {
         this->capacity *= 2;
         this->strings = realloc( this->strings, this->capacity * sizeof( char * ) );
@@ -34,21 +30,15 @@ string_vector_add( struct StringVector *this, const char *begin, const char *end
         this->strings[this->size++] = strndup( begin, end - begin );
 }
 
-size_t
-string_vector_size( const struct StringVector *this )
-{
+size_t string_vector_size( const struct StringVector *this ) {
     return this->size;
 }
 
-char *
-string_vector_get( const struct StringVector *this, size_t index )
-{
+char * string_vector_get( const struct StringVector *this, size_t index ) {
     return this->strings[index];
 }
 
-struct StringVector
-split_line( char *line )
-{
+struct StringVector split_line( char *line ) {
     struct StringVector tokens;
     string_vector_init( &tokens, 8 );
 
