b5ad692ee81368e3b3d8d7450228f82153db210f
ennoel <enzo.noel@etu.u-bordeaux.fr>
2022-11-08 15:26:13 +0100

    add mkdir, rmdir, rm, cp, mv

--- src/actions.c
+++ src/actions.c
@@ -20,13 +20,16 @@ static void do_cd(shell_t *this, const struct StringVector *args)
         dst_dir = getenv("HOME");
     }
     chdir(dst_dir);
-    args;
 }
 
 static void do_pwd(shell_t *this, const struct StringVector *args)
 {
-    // system("pwd");
-    args;
+    pid_t p = fork();
+    if (p == 0) {
+        execvp("pwd", args->strings);
+        exit (EXIT_SUCCESS);
+    }
+    wait(&p);
 }
 
 static void do_help(shell_t *this, const struct StringVector *args)
@@ -45,7 +48,6 @@ static void do_system(shell_t *this, const struct StringVector *args)
         execvp(file, args->strings + 1);
         exit(EXIT_SUCCESS);
     }
-
     wait(&p);
 }
 
@@ -64,6 +66,88 @@ static void do_list(shell_t *this, const struct StringVector *args)
     wait(&p);
 }
 
+static void do_createDir(shell_t *this, const struct StringVector *args)
+{
+    char *dir = string_vector_get(args, 1);
+    if (dir == NULL)
+    {
+        printf("missing argument !\n");
+        return;
+    }
+    pid_t p = fork();
+    if (p == 0) {
+        execvp("mkdir", args -> strings);
+        exit (EXIT_SUCCESS);
+    }
+    wait(&p);
+}
+
+static void do_removeDir(shell_t *this, const struct StringVector *args)
+{
+    char *dir = string_vector_get(args, 1);
+    if (dir == NULL)
+    {
+        printf("missing argument !\n");
+        return;
+    }
+    pid_t p = fork();
+    if (p == 0) {
+        execvp("rmdir", args -> strings);
+        exit (EXIT_SUCCESS);
+    }
+    wait(&p);
+}
+
+static void do_rm(shell_t *this, const struct StringVector *args)
+{
+    char *file = string_vector_get(args, 1);
+    if (file == NULL)
+    {
+        printf("missing argument !\n");
+        return;
+    }
+    pid_t p = fork();
+    if (p == 0) {
+        execvp("rm", args -> strings);
+        exit (EXIT_SUCCESS);
+    }
+    wait(&p);
+}
+
+static void do_cp(shell_t *this, const struct StringVector *args)
+{
+    char *file = string_vector_get(args, 1);
+    char *dst = string_vector_get(args, 2);
+    if (file == NULL || dst == NULL)
+    {
+        printf("missing argument !\n");
+        return;
+    }
+    pid_t p = fork();
+    if (p == 0) {
+        execvp("cp", args -> strings);
+        exit (EXIT_SUCCESS);
+    }
+    wait(&p);
+}
+
+static void do_move(shell_t *this, const struct StringVector *args)
+{
+    char *file = string_vector_get(args, 1);
+    char *dst = string_vector_get(args, 2);
+    if (file == NULL || dst == NULL)
+    {
+        printf("missing argument !\n");
+        return;
+    }
+    pid_t p = fork();
+    if (p == 0) {
+        execvp("mv", args -> strings);
+        exit (EXIT_SUCCESS);
+    }
+    wait(&p);
+}
+
 static struct {
     const char *name;
     Action action;
@@ -75,6 +159,11 @@ static struct {
         { .name = "?",    .action = do_help},
         { .name = "!",    .action = do_system},
         { .name = "ls",   .action = do_list},
+        { .name = "mkdir",.action = do_createDir},
+        { .name = "rmdir",.action = do_removeDir},
+        { .name = "rm",   .action = do_rm},
+        { .name = "cp",   .action = do_cp},
+        { .name = "mv",   .action = do_move},
         { .name = NULL,   .action = do_unknown}
 };
 
